7f195384d61b2041497b923e72dc064b8544d253
Narin Bhadarka <72862500+Najubhai-2031@users.noreply.github.com>
2022-09-21 12:22:43 +0530

    v 1.2
    Handled some Error...

--- src/Component/YourOrder.js
+++ src/Component/YourOrder.js
@@ -1,128 +1,185 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function YourOrder(props) {
 
+    const navigate = useNavigate();
+
     const [order, setOrder] = useState([])
+    const [useridd, setUseridd] = useState('')
 
 
     useEffect(() => {
         let User = JSON.parse(localStorage.getItem('User'))
-        let userId = User.user_id
-
-        const dataa = {
-            user_id: userId
+        if (User === null) {
+            setUseridd('Please Login First')
         }
-        axios.post('https://akashsir.in/myapi/ecom1/api/api-order-listing.php', dataa, {
-            headers: { 'Content-Type': 'multipart/form-data' }
-        })
-            .then(res => setOrder(res.data.order_list))
+        else {
+            let userId = User.user_id
+
+            const dataa = {
+                user_id: userId
+            }
+            axios.post('https://akashsir.in/myapi/ecom1/api/api-order-listing.php', dataa, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            })
+                .then(res => setOrder(res.data.order_list))
+        }
     }, [])
 
-    return (
-        <React.Fragment>
+    if (useridd) {
+        return (
+            <React.Fragment>
 
-            {/* <!-- catg header banner section --> */}
-            <section id="aa-catg-head-banner">
-                <img src="img/fashion/fashion-header-bg-8.jpg" alt="fashion img" />
-                <div className="aa-catg-head-banner-area">
-                    <div className="container">
-                        <div className="aa-catg-head-banner-content">
-                            <h2>My Order</h2>
-                            <ol className="breadcrumb">
-                                <li><a href="index.html">Home</a></li>
-                                <li className="active">Order</li>
-                            </ol>
+                {/* <!-- catg header banner section --> */}
+                <section id="aa-catg-head-banner">
+                    <img src="img/fashion/fashion-header-bg-8.jpg" alt="fashion img" />
+                    <div className="aa-catg-head-banner-area">
+                        <div className="container">
+                            <div className="aa-catg-head-banner-content">
+                                <h2>My Order</h2>
+                                <ol className="breadcrumb">
+                                    <li><a href="index.html">Home</a></li>
+                                    <li className="active">Order</li>
+                                </ol>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            {/* <!-- / catg header banner section --> */}
+                </section>
+                {/* <!-- / catg header banner section --> */}
 
-            {/* <!-- Cart view section --> */}
-            <section id="cart-view">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="cart-view-area">
-                                <div className="cart-view-table">
+                {/* <!-- Cart view section --> */}
+                <section id="cart-view">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="cart-view-area">
+                                    <div className="cart-view-table">
+                                        
+                                            <h1 style={{ textAlign: 'center', fontSize: '40px', color: '#ff6666' }} colSpan={4}>{useridd}</h1>
+                                        
+                                                {/* <!-- Cart Total view --> */}
+                                                <div className="cart-view-total">
+                                                    <a href="#" className="aa-cart-view-btn" onClick={()=>navigate('/Checkout')}>Proced to Checkout</a>
+                                                </div>
+
+                                            </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                </section>
+                {/* <!-- / Cart view section --> */}
+
+            </React.Fragment >
+        )
+    }
+    else {
+        return (
+            <React.Fragment>
+
+                {/* <!-- catg header banner section --> */}
+                <section id="aa-catg-head-banner">
+                    <img src="img/fashion/fashion-header-bg-8.jpg" alt="fashion img" />
+                    <div className="aa-catg-head-banner-area">
+                        <div className="container">
+                            <div className="aa-catg-head-banner-content">
+                                <h2>My Order</h2>
+                                <ol className="breadcrumb">
+                                    <li><a href="index.html">Home</a></li>
+                                    <li className="active">Order</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {/* <!-- / catg header banner section --> */}
+
+                {/* <!-- Cart view section --> */}
+                <section id="cart-view">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="cart-view-area">
+                                    <div className="cart-view-table">
 
 
-                                    {order.map(item =>
-                                        <form action="">
-                                            <div className="table-responsive">
-                                                <table className="table">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>Order id</th>
-                                                            <th>Order Date</th>
-                                                            <th>Order Status</th>
-                                                            <th>Total Amount</th>
-                                                            <th>Shipping Name</th>
-                                                            <th>Shipping Mobile</th>
-                                                            <th>Shipping Address</th>
-                                                            <th>Payment Method</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        <tr>
-                                                            <td>{item.order_id}</td>
-                                                            <td>{item.order_date}</td>
-                                                            <td>{item.order_status}</td>
-                                                            <td>{item.total_amount}</td>
-                                                            <td>{item.shipping_name}</td>
-                                                            <td>{item.shipping_mobile}</td>
-                                                            <td>{item.shipping_address}</td>
-                                                            <td>{item.payment_method}</td>
-                                                        </tr>
-                                                    </tbody>
-                                                    <table border="1px solid" width='100%'>
+                                        {order.map(item =>
+                                            <form action="">
+                                                <div className="table-responsive">
+                                                    <table className="table">
                                                         <thead>
                                                             <tr>
-                                                                <th>Product Name</th>
-                                                                <th>Quantity</th>
-                                                                <th>Amount</th>
+                                                                <th>Order id</th>
+                                                                <th>Order Date</th>
+                                                                <th>Order Status</th>
                                                                 <th>Total Amount</th>
-                                                                <th>Image</th>
+                                                                <th>Shipping Name</th>
+                                                                <th>Shipping Mobile</th>
+                                                                <th>Shipping Address</th>
+                                                                <th>Payment Method</th>
                                                             </tr>
                                                         </thead>
-                                                        {item.order_details.map(item =>
-                                                            <>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>{item.order_id}</td>
+                                                                <td>{item.order_date}</td>
+                                                                <td>{item.order_status}</td>
+                                                                <td>{item.total_amount}</td>
+                                                                <td>{item.shipping_name}</td>
+                                                                <td>{item.shipping_mobile}</td>
+                                                                <td>{item.shipping_address}</td>
+                                                                <td>{item.payment_method}</td>
+                                                            </tr>
+                                                        </tbody>
+                                                        <table border="1px solid" width='100%'>
+                                                            <thead>
+                                                                <tr>
+                                                                    <th>Product Name</th>
+                                                                    <th>Quantity</th>
+                                                                    <th>Amount</th>
+                                                                    <th>Total Amount</th>
+                                                                    <th>Image</th>
+                                                                </tr>
+                                                            </thead>
+                                                            {item.order_details.map(item =>
+                                                                <>
 
-                                                                <tbody>
-                                                                    <tr>
-                                                                        <td><a href="#"><img src={item.product_image} alt="img" /></a></td>
+                                                                    <tbody>
+                                                                        <tr>
+                                                                            <td><a href="#"><img src={item.product_image} alt="img" /></a></td>
 
-                                                                        <td><Link onClick={() => props.quickView(item)} className="aa-cart-title" to='/ProductDetails'>{item.product_name}</Link></td>
-                                                                        <td>₹{item.product_price}</td>
-                                                                    </tr>
-                                                                </tbody>
-                                                            </>
-                                                        )}
+                                                                            <td><Link onClick={() => props.quickView(item)} className="aa-cart-title" to='/ProductDetails'>{item.product_name}</Link></td>
+                                                                            <td>₹{item.product_price}</td>
+                                                                        </tr>
+                                                                    </tbody>
+                                                                </>
+                                                            )}
+                                                        </table>
+
                                                     </table>
-
-                                                </table>
-                                            </div>
-                                        </form>
-                                    )}
+                                                </div>
+                                            </form>
+                                        )}
 
 
-                                    {/* <!-- Cart Total view --> */}
-                                    <div className="cart-view-total">
-                                        <a href="#" className="aa-cart-view-btn">Proced to Checkout</a>
+                                        {/* <!-- Cart Total view --> */}
+                                        <div className="cart-view-total">
+                                            <a href="#" className="aa-cart-view-btn">Proced to Checkout</a>
+                                        </div>
+
                                     </div>
-
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            {/* <!-- / Cart view section --> */}
+                </section>
+                {/* <!-- / Cart view section --> */}
 
-        </React.Fragment >
-    );
+            </React.Fragment >
+        )
+    }
 }
 
 export default YourOrder;
